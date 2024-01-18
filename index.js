3b7b9521e09fa998fd74deffd8583fcc8f622ed7
Shahbi88 <rihabazar@gmail.com>
2024-01-18 19:32:35 +0000

    set up api call functions not working

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-let trigger = document.querySelector(".button");
-trigger.addEventListener("click", generatePoem);
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+  });
+}
 
 function generatePoem(event) {
   event.preventDefault();
-  new Typewriter("#poem", {
-    strings: ["This is your poem"],
-    autoStart: true,
-    cursor: "",
-    delay: 3,
-    loop: false,
-    deleteSpeed: 0,
-  });
+  let apiKey = "d9833o0d576c1t0baaff4f33407e3baa";
+  let context =
+    "you're an expert in poetry with access to Arabic-to-English translated poems. Generate a poem about that";
+  let instructionsInput = document.querySelector("#userInput");
+  let prompt = `generate a poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
+  console.log(apiUrl);
 }
+
+let trigger = document.querySelector(".button");
+trigger.addEventListener("click", generatePoem);
