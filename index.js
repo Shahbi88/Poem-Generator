41ea9ffb1f4556fe2858336421460f52662473cc
Shahbi88 <rihabazar@gmail.com>
2024-01-19 20:30:52 +0000

    larger footer on big screen/women poets more than men

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function generatePoem(event) {
   help.innerHTML = "Finding you a poem now..";
   let apiKey = "d9833o0d576c1t0baaff4f33407e3baa";
   let context =
-    "you're an expert in poetry with access to Arabic-to-English translated poems. Generate an eight-line poem about that. Below the poem, include who it is written by. Don't display poems by anonymous authors.";
+    "you're an expert in poetry with access to Arabic-to-English translated poems. Generate an eight-line poem about that. Below the poem, include who it is written by. Add -Written by: in a new line after the poem, use a line break. Do not fetch poems by anonymous authors.Display works by women poets more than men";
   let instructionsInput = document.querySelector(".userInput");
   let prompt = `generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
