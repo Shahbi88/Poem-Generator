2bfce1e87a96f7924a344ffd453449a096ddf8d8
Shahbi88 <rihabazar@gmail.com>
2024-01-19 23:00:09 +0000

    smaller texts

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function generatePoem(event) {
   help.innerHTML = "Finding you a poem now..";
   let apiKey = "d9833o0d576c1t0baaff4f33407e3baa";
   let context =
-    "you're an expert in poetry with access to exclusively Arabic poems that are translated into English. Generate an eight-line poem about that. Below the poem, include who it is written by. Add -Written by: in a new line after the poem, use a line break. Do not use poems by anonymous or unknown authors. Make your result choices gender-balanced but don't mention the gender of the poet in your credit section.Include poems by poets who are men, women and gender-queer";
+    "you're an expert in poetry with access to exclusively Arabic poems that are translated into English. Generate an eight-line poem about that. Below the poem, include who it is written by. Add -Written by: in a new line after the poem, use a line break. Do not use poems by anonymous or unknown authors. Make your result choices gender-balanced but don't mention the gender of the poet in your credit section.Include poems by poets who are men, women and gender-queer. don't give too many results by Rumi";
   let instructionsInput = document.querySelector(".userInput");
   let prompt = `generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
