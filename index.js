d1062bb4eed810c5a078c0c0e394a717b6ec69fc
Shahbi88 <rihabazar@gmail.com>
2024-01-19 21:29:51 +0000

    improvements to the engine and the display

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  let poemSpace = document.querySelector(".hidden");
   poemSpace.classList.remove("hidden");
   poemSpace.classList.add("poem");
   help.innerHTML = "";
@@ -13,18 +12,19 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
-
+  poemSpace.innerHTML = null;
   help.innerHTML = "Finding you a poem now..";
   let apiKey = "d9833o0d576c1t0baaff4f33407e3baa";
   let context =
-    "you're an expert in poetry with access to Arabic-to-English translated poems. Generate an eight-line poem about that. Below the poem, include who it is written by. Add -Written by: in a new line after the poem, use a line break. Do not fetch poems by anonymous authors.Display works by women poets more than men";
+    "you're an expert in poetry with access to exclusively Arabic poems that are translated into English. Generate an eight-line poem about that. Below the poem, include who it is written by. Add -Written by: in a new line after the poem, use a line break. Do not use poems by anonymous or unknown authors. Make your result choices gender-balanced but don't mention the gender of the poet in your credit section.Include poems by poets who are men, women and gender-queer";
   let instructionsInput = document.querySelector(".userInput");
   let prompt = `generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
+
   console.log(apiUrl);
 }
-
+let poemSpace = document.querySelector(".hidden");
 let trigger = document.querySelector(".button");
 trigger.addEventListener("click", generatePoem);
 let help = document.querySelector(".help");
