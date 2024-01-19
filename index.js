5c8497f9e1558bd8e6739363de494b9faff4e287
Shahbi88 <rihabazar@gmail.com>
2024-01-19 19:19:56 +0000

    mobile friendly

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function generatePoem(event) {
   let apiKey = "d9833o0d576c1t0baaff4f33407e3baa";
   let context =
     "you're an expert in poetry with access to Arabic-to-English translated poems. Generate an eight-line poem about that. Below the poem, include who it is written by. Don't display poems by anonymous authors.";
-  let instructionsInput = document.querySelector("#userInput");
+  let instructionsInput = document.querySelector(".userInput");
   let prompt = `generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
