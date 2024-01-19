b9bda49f28f6df19f491adf78e2fef6cd4976935
Shahbi88 <rihabazar@gmail.com>
2024-01-19 22:05:02 +0000

    styling improvements

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function generatePoem(event) {
 
   console.log(apiUrl);
 }
+
 let poemSpace = document.querySelector(".hidden");
 let trigger = document.querySelector(".button");
 trigger.addEventListener("click", generatePoem);
